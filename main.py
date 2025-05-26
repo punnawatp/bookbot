765e4c2cd9b54fced37cb6347b2944afe89de134
punnawatp <193766920+punnawatp@users.noreply.github.com>
2025-05-26 12:50:01 +0100

    finshed sorted dictionary

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from stats import get_num_words
 from stats import get_dictionary
-from stats import new_dict
+from stats import sort_dict
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,12 +12,10 @@ def main(path):
     text = get_book_text(path)
     print(text)
 
-#main("./books/frankenstein.txt")
 
 frakenstein_text = get_book_text("./books/frankenstein.txt")
 
-#print(get_num_words(frakenstein_text))
+print(get_num_words(frakenstein_text))
 
-#print(get_dictionary(frakenstein_text))
+dictionary = sort_dict(get_dictionary(frakenstein_text))
 
-print(new_dict(get_dictionary(frakenstein_text)))
--- stats.py
+++ stats.py
@@ -8,27 +8,27 @@ def get_num_words(text):
 def get_dictionary(text):
     lower_case_text = text.lower()
     unique_letters = set()
-    
     for char in lower_case_text:
         unique_letters.add(char)
-
     dictionary = {}
-
     for uni_char in unique_letters:
         counter = 0
         for char in lower_case_text:
             if char == uni_char:
                 counter += 1
         dictionary[uni_char] = counter
-    
     return dictionary
 
 
-def new_dict(dict):
+def sort_dict(dict):
     result = []
     for c, n in dict.items():
         new_dict = {}
         new_dict["char"] = c
         new_dict["num"] = n
         result.append(new_dict)
+    def sort_on(new_dict):
+        return new_dict["num"]
+    result.sort(reverse = True, key = sort_on)
     return result
+
