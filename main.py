a20e363201b5341baebe9852df5653188fe9b1d3
punnawatp <193766920+punnawatp@users.noreply.github.com>
2025-07-08 14:20:44 +0700

    upgrade boot.dev and reooptimize count char dictionary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import count_char
 
 def get_book_text(path):
     with open(path) as f:
@@ -9,6 +10,6 @@ def main():
     text = get_book_text(path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    print(count_char(text))
 
-
-
+main()
--- stats.py
+++ stats.py
@@ -5,4 +5,14 @@ def get_num_words(text):
         counter += 1
     return f"{counter} words found in the document"
 
+def count_char(text):
+    lowered = text.lower()
+    dict = {}
+    for c in lowered:
+        if c not in dict:
+            dict[c] = 1
+        else:
+            dict[c] += 1
+    return dict
+         
 
