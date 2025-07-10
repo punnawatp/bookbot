9668ca47706b9c8101180cc7e1a38df38240f599
punnawatp <193766920+punnawatp@users.noreply.github.com>
2025-07-10 17:37:15 +0700

    add sys.arg and sys.exit(1) for error missing arg

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from stats import get_num_words
 from stats import count_char
 from stats import get_new_list
+import sys
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def main():
-    path = "./books/frankenstein.txt"
-    text = get_book_text(path)
-    num_words = get_num_words(text)
-    print(f"Found {num_words} total words")
-    dict = count_char(text)
-    new_list = get_new_list(dict)
-    for word in new_list:
-        print(word["char"] + ":", word["num"])
-
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path = sys.argv[1]
+        text = get_book_text(path)
+        num_words = get_num_words(text)
+        print(f"Found {num_words} total words")
+        dict = count_char(text)
+        new_list = get_new_list(dict)
+        for word in new_list:
+            print(word["char"] + ":", word["num"])
+    
 main()
